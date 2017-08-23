183609cbfa91cfbfed7d805a321fe7278c79c41f
Daniel Lebrero <dlebrero@gmail.com>
2017-08-23 15:52:09 +0200

    copying over the code from akvo-lumen

---
+++ postgres/provision/helpers/create-a-database.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE test_database
+WITH OWNER = anybody
+     TEMPLATE = template0
+     ENCODING = 'UTF8'
+     LC_COLLATE = 'en_US.UTF-8'
+     LC_CTYPE = 'en_US.UTF-8';
---
+++ postgres/provision/setup.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -e
+
+CLI_ERR_MSG="Postgres CLI tools not available (psql). Using Postgres.app, look
+at http://postgresapp.com/documentation/cli-tools.html. Aborting."
+hash psql 2>/dev/null || { echo >&2 $CLI_ERR_MSG ; exit 1; }
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd $DIR
+
+
+# Provision
+
+psql -c "CREATE ROLE anybody WITH PASSWORD 'password' CREATEDB LOGIN;"
+
+psql -f $DIR/helpers/create-a-database.sql
+
+psql -d test_database -f $DIR/helpers/create-extensions.sql
+
+sh -c "$DIR/helpers/import_csv.sh $DIR/helpers/liberia.csv | psql -d test_database";
+echo "SELECT AddGeometryColumn('liberia', 'geom', 4326, 'point', 2);" \
+     "UPDATE liberia SET geom = " \
+     "  ST_SetSRID(ST_MakePoint(longitude::float8, latitude::float8), 4326)" \
+     " WHERE longitude != '' and latitude != '';" \
+     "CREATE INDEX ON liberia USING GIST (geom);" \
+     | psql --set ON_ERROR_STOP=1 -d test_database
+
+echo "ALTER TABLE liberia OWNER TO anybody;" | psql -d test_database
+echo "ALTER TABLE spatial_ref_sys OWNER TO test_database;" | psql -d test_database
+
+echo ""
+echo "----------"
+echo "Done!!"
