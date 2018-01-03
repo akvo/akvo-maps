f6c263a7e1d13cbdc0247b8d66b1ee67f374a8e5
Dan Lebrero <dlebrero@gmail.com>
2018-01-03 20:30:54 +0100

    Adding timeout to tests

--- end-to-end-tests/test/windshaft_test/core_test.clj
+++ end-to-end-tests/test/windshaft_test/core_test.clj
@@ -22,29 +22,35 @@
 
 (def connection-pool (clj-http.conn-mgr/make-reusable-conn-manager {:timeout 20 :threads 8 :default-per-route 8}))
 
+(def default-http-opts
+  {:socket-timeout       10000
+   :conn-timeout         10000
+   :conn-request-timeout 10000
+   :as :json
+   :connection-manager connection-pool})
+
 (def first-instance "http://windshaft:4000/layergroup")
 (def second-instance "http://windshaft2:4000/layergroup")
 
 (defn create-map [last-db-update]
   (http/post first-instance
-             {:as                 :json
-              :connection-manager connection-pool
-              :headers            {"content-type"     "application/json"
-                                   "x-db-host"        "postgres"
-                                   "x-db-user"        "anybody"
-                                   "x-db-password"    "password"
-                                   "x-db-name"        "test_database"
-                                   "x-db-last-update" last-db-update
-                                   "x-db-port"        "5432"}
-              :body               (json/generate-string
-                                    {:version "1.5.0",
-                                     :layers  [{:type    "mapnik",
-                                                :options {:sql              "select instance, geom, yearcons::integer as yearcons from liberia where yearcons ~ '^\\d{4}$';",
-                                                          :geom_column      "geom",
-                                                          :srid             4326,
-                                                          :cartocss         "#s { marker-width: 5; marker-fill:#f45; marker-line-color:#813; marker-allow-overlap:true; marker-fill-opacity: 0.3;} #s[yearcons>=2009] {marker-fill: #1F78B4; marker-line-color: #0000FF;}",
-                                                          :cartocss_version "2.0.0",
-                                                          :interactivity    "instance"}}]})}))
+             (merge default-http-opts
+                    {:headers {"content-type"     "application/json"
+                               "x-db-host"        "postgres"
+                               "x-db-user"        "anybody"
+                               "x-db-password"    "password"
+                               "x-db-name"        "test_database"
+                               "x-db-last-update" last-db-update
+                               "x-db-port"        "5432"}
+                     :body    (json/generate-string
+                                {:version "1.5.0",
+                                 :layers  [{:type    "mapnik",
+                                            :options {:sql              "select instance, geom, yearcons::integer as yearcons from liberia where yearcons ~ '^\\d{4}$';",
+                                                      :geom_column      "geom",
+                                                      :srid             4326,
+                                                      :cartocss         "#s { marker-width: 5; marker-fill:#f45; marker-line-color:#813; marker-allow-overlap:true; marker-fill-opacity: 0.3;} #s[yearcons>=2009] {marker-fill: #1F78B4; marker-line-color: #0000FF;}",
+                                                      :cartocss_version "2.0.0",
+                                                      :interactivity    "instance"}}]})})))
 
 (deftest happy-path
          (let [last-db-update 10000
@@ -54,19 +60,19 @@
            (clojure.test/is (not (clojure.string/blank? layer-group)))
            (clojure.test/is (clojure.string/includes? layer-group ":1000"))
 
-           (let [tile (http/get (str first-instance "/" layer-group "/0/0/0/0.grid.json") {:as :json :connection-manager connection-pool})]
+           (let [tile (http/get (str first-instance "/" layer-group "/0/0/0/0.grid.json") default-http-opts)]
              (clojure.test/is (= 200 (:status tile)))
              (clojure.test/is (= "max-age=31536000" (get-in tile [:headers "Cache-Control"]))))
 
-           (let [png (http/get (str first-instance "/" layer-group "/10/483/493.png") {:connection-manager connection-pool})]
+           (let [png (http/get (str first-instance "/" layer-group "/10/483/493.png") (dissoc default-http-opts :as))]
              (clojure.test/is (= 200 (:status png))))))
 
 (deftest redis-caching-works
          (let [response (create-map (System/currentTimeMillis))
                layer-group (-> response :body :layergroupid)]
            (clojure.test/is (= 200 (:status response)))
-           (let [tile-first-instance (http/get (str first-instance "/" layer-group "/0/11/966/990.grid.json") {:as :json :connection-manager connection-pool})
-                 tile-second-instance (http/get (str second-instance "/" layer-group "/0/11/966/990.grid.json") {:as :json :connection-manager connection-pool})]
+           (let [tile-first-instance (http/get (str first-instance "/" layer-group "/0/11/966/990.grid.json") default-http-opts)
+                 tile-second-instance (http/get (str second-instance "/" layer-group "/0/11/966/990.grid.json") default-http-opts)]
              (clojure.test/is (= (:body tile-first-instance)
                                  (:body tile-second-instance))))))
 
@@ -77,5 +83,3 @@
 
     (clojure.test/is (= (-> response-1 :body :layergroupid)
                         (-> response-2 :body :layergroupid)))))
-
-
